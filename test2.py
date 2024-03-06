ef9b75edff0e5aebb94d5d864bbe11159a04ba1b
ramiz-moktader <67529752+ramiz-moktader@users.noreply.github.com>
2024-03-06 10:59:44 +0600

    Update test2.py

--- test2.py
+++ test2.py
@@ -9,47 +9,22 @@ def main():
         """
         <style>
         body, .stSelectbox, .stTextInput, .stButton, .stMarkdown, .stSmall, .stText, .stError, .stHeader, .stImage, .stProgressBar {
-            font-size: 16px;  /* Adjust the font size as needed */
+            font-size: 16px; /* Adjust the font size as needed */
             font-family: 'Times New Roman', Times, serif; /* Use Times New Roman font */
         }
         .stMarkdown a {
-            font-size: 16px;  /* Adjust the font size for links as needed */
+            font-size: 16px; /* Adjust the font size for links as needed */
         }
         .css-1v3fvcr {
-            font-size: 16px;  /* Adjust the font size for Streamlit data frame text as needed */
+            font-size: 16px; /* Adjust the font size for Streamlit data frame text as needed */
         }
         </style>
         """,
         unsafe_allow_html=True
     )
-    
-    # # Slider example
-    # num = st.slider("Choose a number", 1, 10, key="slider")
-    # st.write(st.session_state)
 
     # Title and markdown examples
     st.title("Excel File Reader")
-    # st.markdown("*Streamlit* is **really** ***cool***.")
-    # st.markdown(':red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in] :gray[pretty] :rainbow[colors].')
-    # st.markdown("Here's a bouquet &mdash; :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
-
-    # # Multi-line markdown example
-    # multi = '''
-    #     ### Hello welcome 
-    #     Today we will talk about 
-    #     2023-12-18
-    #     Expanded access to AI coding has arrived in Colab across 175 locales for all tiers of Colab users
-    #     Improvements to display of ML-based inline completions (for eligible Pro/Pro+ users)
-    #     Started a series of notebooks highlighting Gemini API capabilities
-    #     Enable ⌘/Ctrl+L to select the full line in an editor
-    #     Fixed bug where we weren't correctly formatting output from multiple execution results
-    #     * List item
-    #     ```
-    #     # This is formatted as code
-    #     ```
-    #     * List item
-    # '''
-    # st.markdown(multi)
 
     # Upload Excel file through Streamlit
     uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx", "xls"])
@@ -65,54 +40,61 @@ def main():
         st.write("Selected Sheet:")
         st.write(df[sheet_option])
 
-        # Option to choose a column for describing
-        describe_column_option = st.selectbox("Select a column for describe", list(df[sheet_option].columns))
+        # Option to choose multiple columns for describing in the first column
+        st.markdown("""
+    <style>
+        .stMultiSelect [data-baseweb=select] span{
+            max-width: 700px;
+            font-size: 0.9rem;
+        }
+    </style>
+    """, unsafe_allow_html=True)
 
-        # Describe the selected column
-        st.write("Description of Selected Column:")
-        st.write(df[sheet_option][describe_column_option].describe())
+        selected_columns = st.multiselect("Select columns for value counts", list(df[sheet_option].columns))
+           
+        if selected_columns:
+            # Calculate value counts and percentage for each selected column
+            main_value_counts_df = pd.DataFrame(columns=['Column', 'Value', 'Percentage', 'Count'])
+            for col in selected_columns:
+                value_counts = df[sheet_option][col].value_counts()
+                total_rows = len(df[sheet_option])
+                percentage = (value_counts / total_rows) * 100
 
-        # Option to choose a column for value counts
-        value_counts_column_option = st.selectbox("Select a column for value counts", list(df[sheet_option].columns))
+                # Create a DataFrame for the current column
+                col_value_counts_df = pd.DataFrame({'Value': value_counts.index, 'Percentage': percentage.values, 'Count': value_counts.values})
 
-        # Calculate value counts for the selected column
-        value_counts = df[sheet_option][value_counts_column_option].value_counts()
+                # Add column name as the first row
+                col_value_counts_df = pd.concat([pd.DataFrame({'Column': [col]}), col_value_counts_df], axis=1)
 
-        # Calculate percentage for each row
-        total_rows = len(df[sheet_option])
-        percentage = (value_counts / total_rows) * 100
+                # Add a row for total sum
+                total_count = col_value_counts_df['Count'].sum()
+                total_percentage = col_value_counts_df['Percentage'].sum()
+                total_row = pd.DataFrame({'Column': ['Total'], 'Value': ['Total'], 'Percentage': [total_percentage], 'Count': [total_count]})
 
-        # Create a DataFrame with value counts and percentage
-        value_counts_df = pd.DataFrame({'Value': value_counts.index, 'Count': value_counts.values, 'Percentage': percentage.values})
+                # Concatenate total row to the current column's DataFrame
+                col_value_counts_df = pd.concat([col_value_counts_df, total_row])
 
-        # Add column names as the first row
-        columns_row = pd.DataFrame({'Value': [value_counts_column_option,]
-                                     })
-        
-        # Concatenate column names row to the DataFrame
-        value_counts_df = pd.concat([columns_row, value_counts_df])
+                # Append the current column's DataFrame to the main DataFrame
+                main_value_counts_df = pd.concat([main_value_counts_df, col_value_counts_df])
 
-        # Add a row for total sum
-        total_count = value_counts_df['Count'].sum()
-        total_percentage = value_counts_df['Percentage'].sum()
-        total_row = pd.DataFrame({'Value': ['Total'], 'Count': [total_count], 'Percentage': [total_percentage]})
+                # Display value counts and percentage for the selected columns
+                st.write(f"Value Counts and Percentage for '{col}':")
+                st.write(col_value_counts_df)
 
-        # Concatenate total row to the DataFrame
-        value_counts_df = pd.concat([value_counts_df, total_row])
+        # Display value counts and percentage for all selected columns in a single DataFrame
+        if not main_value_counts_df.empty:
+            st.write("Value Counts and Percentage for All Selected Columns:")
+            st.write(main_value_counts_df)
 
-        # Display value counts and percentage for the selected column
-        st.write("Value Counts and Percentage of Selected Column:")
-        st.write(value_counts_df)
+            # Add a download button for the main DataFrame as Excel
+            if st.button("Download as Excel"):
+                # Prepare the Excel file as binary data
+                excel_binary = io.BytesIO()
+                with pd.ExcelWriter(excel_binary, engine='openpyxl', mode='w') as writer:
+                    main_value_counts_df.to_excel(writer, sheet_name=sheet_option, index=False)
 
-        # Add a download button for the selected sheet as Excel
-        if st.button("Download as Excel"):
-            # Prepare the Excel file as binary data
-            excel_binary = io.BytesIO()
-            with pd.ExcelWriter(excel_binary, engine='openpyxl', mode='w') as writer:
-                value_counts_df.to_excel(writer, sheet_name=sheet_option, index=False)
-
-            # Trigger the download of the Excel file
-            st.download_button(label="Download Now", data=excel_binary, file_name=f"{uploaded_file.name}_{sheet_option}_output.xlsx", key="download_button")
+                # Trigger the download of the Excel file
+                st.download_button(label="Download Now", data=excel_binary, file_name=f"{uploaded_file.name}_{sheet_option}_output.xlsx", key="download_button")
 
 # Call the main function
 if __name__ == "__main__":
